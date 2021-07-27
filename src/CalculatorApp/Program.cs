a68b9bb11ea227d7bd538fb45c10c039b7273826
Agustin Jose <agustinwawrzyk@protonmail.com>
2021-07-27 12:24:02 -0300

    Tasks CalculatorApp

---
+++ src/CalculatorApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 23;
+            int y = 2;
+
+            //1. Create abstract CalculatorBase class with Add, Substract, Multiply and Divide functionality
+            //2. create derived SimpleCalculator class of the base class you just created and make the calculations with given X and Y values
+            //3. Create IAdvancedCalculator interface with SquareRoot, Square, Cube functionalities
+            //4. Create derived AdvancedCalculator class of CalculatorBase and IAdvancedCalculator structures
+        }
+    }
+}
