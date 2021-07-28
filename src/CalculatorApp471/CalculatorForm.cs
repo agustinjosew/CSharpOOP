02903b6840015b7090df1487e12d6019f819d6ec
Agustin Jose <agustinwawrzyk@protonmail.com>
2021-07-28 17:55:26 -0300

    Delegate & Events

---
+++ src/CalculatorApp471/CalculatorForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApp
+{
+    public partial class CalculatorForm : Form
+    {
+        //delegate definitions
+        public delegate int    Add      (int x ,int y);
+        public delegate int    Subtract (int x ,int y);
+        public delegate double Sqrt     (int x);
+        public delegate long   Square   (int x);
+
+        //fields to hold instances of this delegates
+        public Add      AddDelegate;
+        public Subtract SubtractDelegate;
+        public Sqrt     SqrtDelegate;
+        public Square   SquareDelegate;
+
+        //properties
+        private int x
+        {
+            get
+            {
+                return ( int ) xInputBox.Value;
+            }
+        }
+
+        private int y
+        {
+            get
+            {
+                return ( int ) yInputBox.Value;
+            }
+        }
+
+
+
+        public CalculatorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender ,EventArgs e)
+        {
+            ResultTextBox.Text = AddDelegate(x,y).ToString();
+        }
+
+        private void SubtractButton_Click(object sender ,EventArgs e)
+        {
+            ResultTextBox.Text = SubtractDelegate(x,y).ToString();
+        }
+
+        private void SquareButton_Click(object sender ,EventArgs e)
+        {
+            ResultTextBox.Text = SquareDelegate(x).ToString();
+        }
+
+        private void SquareRootButton_Click(object sender ,EventArgs e)
+        {
+            ResultTextBox.Text = SqrtDelegate(x).ToString();
+        }
+    }
+}
--- src/CalculatorApp471/Program.cs
+++ src/CalculatorApp471/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CalculatorApp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,21 +21,30 @@ namespace CalculatorApp471
 
             Calculator myCalculator = new Calculator();
 
-            int   resultOne   = myCalculator.Add(x,y);
-            int   resultTwo   = myCalculator.Substract(x,y);
-            float resultThree = myCalculator.Divide(x,y);
-            Int64 resultFour  = myCalculator.Multiply(x,y);
+            //int   resultOne   = myCalculator.Add(x,y);
+            //int   resultTwo   = myCalculator.Substract(x,y);
+            //float resultThree = myCalculator.Divide(x,y);
+            //Int64 resultFour  = myCalculator.Multiply(x,y);
 
             AdvancedCalculator myAdvancedCalculator = new AdvancedCalculator();
 
-            resultOne = myCalculator.Add(x ,y);
-            resultTwo = myCalculator.Substract(x ,y);
-            resultThree = myCalculator.Divide(x ,y);
-            resultFour = myCalculator.Multiply(x ,y);
+            //resultOne = myCalculator.Add(x ,y);
+            //resultTwo = myCalculator.Substract(x ,y);
+            //resultThree = myCalculator.Divide(x ,y);
+            //resultFour = myCalculator.Multiply(x ,y);
 
-            long   resultFive  = myAdvancedCalculator.Cube(x);
-            long   resultSix   = myAdvancedCalculator.Square(x);
-            double resultSeven = myAdvancedCalculator.SquareRoot(x);
+            //long   resultFive  = myAdvancedCalculator.Cube(x);
+            //long   resultSix   = myAdvancedCalculator.Square(x);
+            //double resultSeven = myAdvancedCalculator.SquareRoot(x);
+
+            CalculatorForm myCalculatorForm = new CalculatorForm();
+
+            myCalculatorForm.AddDelegate      = new CalculatorForm.Add(myCalculator.Add);
+            myCalculatorForm.SubtractDelegate = new CalculatorForm.Subtract(myCalculator.Substract);
+            myCalculatorForm.SquareDelegate   = new CalculatorForm.Square(myAdvancedCalculator.Square);
+            myCalculatorForm.SqrtDelegate     = new CalculatorForm.Sqrt(myAdvancedCalculator.SquareRoot);
+
+            myCalculatorForm.ShowDialog();
         }
     }
 }
